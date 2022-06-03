42b93625aa90c896ba6c3e17e6e6261e4da34230
Divya-256 <76658875+Divya-256@users.noreply.github.com>
2022-06-03 11:08:56 +0530

    alignment

--- image.py
+++ image.py
@@ -39,32 +39,32 @@ def preview():
     prev_lbl.pack()
     prev.mainloop()
 
-# chooseL=Label(App,text='Select your image: ')
-# chooseL.grid(row=0,column=0,pady=10)
+chooseL=Label(App,text='Select your image: ')
+chooseL.grid(row=0,column=0,pady=10)
 
-# chooseB=Button(App,text='Choose',command=open_img)
-# chooseB.grid(row=0,column=1,pady=10)
+chooseB=Button(App,text='Choose',command=open_img)
+chooseB.grid(row=0,column=1,pady=10)
 
-# sizeL=Label(App,text='Select a size for your icon')
-# sizeL.grid(row=1,column=0,pady=10)
+sizeL=Label(App,text='Select a size for your icon')
+sizeL.grid(row=1,column=0,pady=10)
 
-# ico_size=IntVar()
-# size_options=[16,24,32,48,64,128,255]
-# ico_size.set(32)
-# size_menu=OptionMenu(App,ico_size,*size_options)
-# size_menu.grid(row=1,column=1)
+ico_size=IntVar()
+size_options=[16,24,32,48,64,128,255]
+ico_size.set(32)
+size_menu=OptionMenu(App,ico_size,*size_options)
+size_menu.grid(row=1,column=1)
 
-# fnameL=Label(App,text='Enter the icon name: ')
-# fnameL.grid(row=2,column=0,pady=10)
+fnameL=Label(App,text='Enter the icon name: ')
+fnameL.grid(row=2,column=0,pady=10)
 
-# ico_name =Entry(App)
-# ico_name.grid(row=2,column=1,pady=10)
+ico_name =Entry(App)
+ico_name.grid(row=2,column=1,pady=10)
 
-# convB=Button(App,text='Convert',command=conv_img)
-# convB.grid(row=3,column=0,pady=10)
+convB=Button(App,text='Convert',command=conv_img)
+convB.grid(row=3,column=0,pady=10)
 
-# prevB=Button(App,text='Preview',command=preview)
-# prevB.grid(row=3,column=1,pady=10)
+prevB=Button(App,text='Preview',command=preview)
+prevB.grid(row=3,column=1,pady=10)
 
 
 App.mainloop()
